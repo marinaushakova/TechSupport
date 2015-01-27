691fa6ab28233448a90a304f1974f69addc54d3e
Marina Ushakova <mushako1@my.westga.edu>
2015-01-26 20:52:41 -0800

    Created module class Incident and DAL class OpenIncidentDBConnection.

---
+++ TechSupportData/DAL/OpenIncidentsDBConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TechSupportData.DAL
+{
+    class OpenIncidentsDBConnection
+    {
+        public static SqlConnection GetConnection()
+        {
+            string connectionString =
+                "Data Source=(local);Initial Catalog=OpenIncidents;" +
+                "Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
---
+++ TechSupportData/Model/Incident.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSupportData.Model
+{
+    class Incident
+    {
+        private int incidentID;
+        private int customerID;
+        private string productCode;
+        private int? techID;
+        private DateTime dateOpend;
+        private DateTime? dateClosed;
+        private string title;
+        private string description;
+
+        public Incident()
+        {
+        }
+
+        public string incidentID { get; set; }
+        public string customerID { get; set; }
+        public string productCode { get; set; }
+        public string techID { get; set; }
+        public string dateOpend { get; set; }
+        public string dateClosed { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+    }
+}

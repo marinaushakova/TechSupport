1c891098dfd1cf6e712f536d621805a6d5e9b04c
Marina Ushakova <mushako1@my.westga.edu>
2015-01-26 20:12:47 -0800

    Initial commit. Main form and OpenIncidentsForm are created.

---
+++ TechSupport/MainForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace TechSupport
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainFormMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.incidentsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.displayOpenIncidentsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mainFormMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainFormMenu
+            // 
+            this.mainFormMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.incidentsToolStripMenuItem});
+            this.mainFormMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainFormMenu.Name = "mainFormMenu";
+            this.mainFormMenu.Size = new System.Drawing.Size(763, 24);
+            this.mainFormMenu.TabIndex = 1;
+            this.mainFormMenu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // incidentsToolStripMenuItem
+            // 
+            this.incidentsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.displayOpenIncidentsToolStripMenuItem});
+            this.incidentsToolStripMenuItem.Name = "incidentsToolStripMenuItem";
+            this.incidentsToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.incidentsToolStripMenuItem.Text = "Incidents";
+            // 
+            // displayOpenIncidentsToolStripMenuItem
+            // 
+            this.displayOpenIncidentsToolStripMenuItem.Name = "displayOpenIncidentsToolStripMenuItem";
+            this.displayOpenIncidentsToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.displayOpenIncidentsToolStripMenuItem.Text = "Display Open Incidents";
+            this.displayOpenIncidentsToolStripMenuItem.Click += new System.EventHandler(this.displayOpenIncidentsToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 330);
+            this.Controls.Add(this.mainFormMenu);
+            this.IsMdiContainer = true;
+            this.Name = "MainForm";
+            this.Text = "TechSupport";
+            this.mainFormMenu.ResumeLayout(false);
+            this.mainFormMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mainFormMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem incidentsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem displayOpenIncidentsToolStripMenuItem;
+    }
+}
+
---
+++ TechSupport/MainForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TechSupport
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        OpenIncidentForm openIncidentsForm;
+
+        private void displayOpenIncidentsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openIncidentsForm == null) {
+                openIncidentsForm = new OpenIncidentForm();
+                openIncidentsForm.MdiParent = this;
+                openIncidentsForm.FormClosed += new FormClosedEventHandler(openIncidentsForm_FormClosed);
+                openIncidentsForm.Show();
+            }
+            else
+            {
+                openIncidentsForm.Activate();
+            }
+            
+        }
+
+        void openIncidentsForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            openIncidentsForm = null;
+        }
+    }
+}

457329273476ee451505414c81d006a426f2b7f3
Marina Ushakova <mushako1@my.westga.edu>
2015-03-15 20:25:48 -0700

    Added expression in the total line of the report counting a number of incidents

--- TechSupport/View/OpenedIncidentsReportForm.Designer.cs
+++ TechSupport/View/OpenedIncidentsReportForm.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
             this.techSupportDataSet = new TechSupport.TechSupportDataSet();
             this.openIncidentsAssignedToTechBindingSource = new System.Windows.Forms.BindingSource(this.components);
@@ -41,13 +41,13 @@
             // reportViewer1
             // 
             this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            reportDataSource1.Name = "OpenIncidentsAssignedToTechDataSet";
-            reportDataSource1.Value = this.openIncidentsAssignedToTechBindingSource;
-            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            reportDataSource2.Name = "OpenIncidentsAssignedToTechDataSet";
+            reportDataSource2.Value = this.openIncidentsAssignedToTechBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource2);
             this.reportViewer1.LocalReport.ReportEmbeddedResource = "TechSupport.OpenIncidentsAssignedToTechReport.rdlc";
             this.reportViewer1.Location = new System.Drawing.Point(0, 0);
             this.reportViewer1.Name = "reportViewer1";
-            this.reportViewer1.Size = new System.Drawing.Size(596, 370);
+            this.reportViewer1.Size = new System.Drawing.Size(797, 438);
             this.reportViewer1.TabIndex = 0;
             // 
             // techSupportDataSet
@@ -68,9 +68,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(596, 370);
+            this.ClientSize = new System.Drawing.Size(797, 438);
             this.Controls.Add(this.reportViewer1);
             this.Name = "OpenedIncidentsReportForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "OpenedIncidentsReportForm";
             this.Load += new System.EventHandler(this.OpenedIncidentsReportForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.techSupportDataSet)).EndInit();
